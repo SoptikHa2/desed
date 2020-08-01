6d309da8aa1f6ff2539d2e0cdc8ea5f3405e2632
Petr Šťastný <petr@soptik.tech>
2020-08-02 01:50:29 +0200

    Added sed annotation parsing and first test

--- src/sed/parser.rs
+++ src/sed/parser.rs
@@ -16,13 +16,13 @@ pub struct SedAnnotation<'a> {
 
 const SED_ANNOTATION_INSTRUCTION_COUNT: usize = 7;
 const SED_ANNOTATION_INSTRUCTIONS: [&str; SED_ANNOTATION_INSTRUCTION_COUNT] = [
-    "SED PROGRAM:\n",
-    "INPUT:   ",
-    "PATTERN: ",
-    "COMMAND:               ",
-    "HOLD:    ",
-    "MATCHED REGEX REGISTERS\n",
-    "END-OF-CYCLE:\n"
+    "\nSED PROGRAM:\n",
+    "\nINPUT:   ",
+    "\nPATTERN: ",
+    "\nCOMMAND:               ",
+    "\nHOLD:    ",
+    "\nMATCHED REGEX REGISTERS\n",
+    "\nEND-OF-CYCLE:\n"
 ];
 
 pub struct SedAnnotationParser {}
@@ -33,43 +33,43 @@ impl SedAnnotationParser {
 
     /// Traverse input text until a next instruction, which has to be on a newline.
     /// 
+    /// All instructions are set to begin with a newline. However, there is a flag: ignore_preceding_newline,
+    /// (this should be used on *first* parse of the sed output), which suppresses the need to have a newline
+    /// at the beginning.
+    /// 
     /// Output is a tuple consisting of:
     /// 1. String from start of input until the next instruction (EXCLUDING the instruction)
     /// 2. The instruction matched
     /// 3. The string AFTER instruction (EXCLUDING the instruction itself and EXCLUDING newline at end of the instruction, if any)
-    fn get_text_until_next_instruction<'a>(input: &'a str) -> Result<(&'a str, Option<&'a str>, &'a str)> {
+    fn get_text_until_next_instruction<'a>(input: &'a str, ignore_preceding_newline: bool) -> Result<(&'a str, Option<&'a str>, &'a str)> {
         // This represents where the selected sed annotation instruction starts
         let mut pattern_end: usize = 0;
         // This represents how many characters of individual sed annotation instructions were actually matched
-        let mut correct_characters_per_annotation: [usize; SED_ANNOTATION_INSTRUCTION_COUNT] = [0; SED_ANNOTATION_INSTRUCTION_COUNT];
+        let mut correct_characters_per_annotation: [usize; SED_ANNOTATION_INSTRUCTION_COUNT] = 
+            [ if ignore_preceding_newline { 1 } else { 0 }; SED_ANNOTATION_INSTRUCTION_COUNT];
         let mut selected_annotation: Option<&str> = None;
 
-        // Go through characters and stop as soon as we find a sed annotation instruction in there.
-        for (character_idx, input_character) in input.chars().enumerate() {
-            for (i, instruction) in SED_ANNOTATION_INSTRUCTIONS.iter().enumerate() {
-                // Check if for current annotation, the next character matches the character of input
-                if let Some(instruction_character) = instruction.chars().skip(correct_characters_per_annotation[i]).next() {
-                    if instruction_character == input_character {
-                        correct_characters_per_annotation[i] += 1;
-                    } else {
-                        // If the next character *didn't* match, check if at least
-                        // the first one matches, and set number of correct matches to zero or one
-                        // accordingly.
-                        if let Some(instruction_character) = instruction.chars().next() {
-                            if instruction_character == instruction_character {
-                                correct_characters_per_annotation[i] = 1;
-                            } else {
-                                correct_characters_per_annotation[i] = 0;
-                            }
-                        } else {
-                            unreachable!();
-                        }
+        // For each character in the input...
+        'outer: for (input_char_idx, input_char) in input.chars().enumerate() {
+            // Check if it matches any sed instruction
+            for (sed_instruction_idx, sed_instruction) in SED_ANNOTATION_INSTRUCTIONS.iter().enumerate() {
+                // Unwrap: assuming there are no zero-size annotation instructions, this function exits the moment any
+                // instruction reaches the end, so this code cannot run if it were to exceed instruction length
+                if sed_instruction.chars().skip(correct_characters_per_annotation[sed_instruction_idx]).next().unwrap() == input_char {
+                    correct_characters_per_annotation[sed_instruction_idx] += 1;
+                    if correct_characters_per_annotation[sed_instruction_idx] == sed_instruction.len() {
+                        selected_annotation = Some(&sed_instruction[1..]);
+                        pattern_end = input_char_idx + 1;
+                        break 'outer;
                     }
-                }
-                // Check if we matched something
-                if correct_characters_per_annotation[i] == instruction.len() {
-                    selected_annotation = Some(instruction);
-                    pattern_end = character_idx;
+                } else {
+                    // If this fails, check if at least the first character matches
+                    // Unwrap: there are no zero-sized instructions, and we check just the first character.
+                    if sed_instruction.chars().next().unwrap() == input_char {
+                        correct_characters_per_annotation[sed_instruction_idx] = 1;
+                    } else {
+                        correct_characters_per_annotation[sed_instruction_idx] = 0;
+                    }
                 }
             }
         }
@@ -91,31 +91,31 @@ mod parser_tests {
     #[test]
     fn split_text_by_instructions() {
         let input = "SED PROGRAM:\nabc\ndef\nghiINPUT:   WRONG_INPUT\nINPUT:   correct\ninput\nEND-OF-CYCLE:\nend";
-        let (before, instr, after) = SedAnnotationParser::get_text_until_next_instruction(&input).unwrap();
+        let (before, instr, after) = SedAnnotationParser::get_text_until_next_instruction(&input, true).unwrap();
         assert_eq!("", before, "Before should be empty, as literally the first thing in test string is instruction.");
         assert_eq!(Some("SED PROGRAM:\n"), instr);
         assert_eq!("abc\ndef\nghiINPUT:   WRONG_INPUT\nINPUT:   correct\ninput\nEND-OF-CYCLE:\nend", after);
 
-        let (before, instr, after) = SedAnnotationParser::get_text_until_next_instruction(&after).unwrap();
-        assert_eq!("\nabc\ndef\nghiINPUT:   WRONG_INPUT\n", before, "Before should contain string (with the most interesting part being ghiINPUT:     ) to test that
+        let (before, instr, after) = SedAnnotationParser::get_text_until_next_instruction(&after, false).unwrap();
+        assert_eq!("abc\ndef\nghiINPUT:   WRONG_INPUT\n", before, "Before should contain string (with the most interesting part being ghiINPUT:     ) to test that
         identifiers are only matched on newlines or string start. If it doesn't have the INPUT:     , then the annotation instruction was parsed as instruction
         even if it was not on new line itself.");
         assert_eq!(Some("INPUT:   "), instr);
         assert_eq!("correct\ninput\nEND-OF-CYCLE:\nend", after);
 
-        let (before, instr, after) = SedAnnotationParser::get_text_until_next_instruction(&after).unwrap();
+        let (before, instr, after) = SedAnnotationParser::get_text_until_next_instruction(&after, false).unwrap();
         assert_eq!("correct\ninput\n", before);
         assert_eq!(Some("END-OF-CYCLE:\n"), instr);
         assert_eq!("end", after);
 
-        let (before, instr, after) = SedAnnotationParser::get_text_until_next_instruction(&after).unwrap();
-        assert_eq!("end", before);
-        assert_eq!(None, instr);
-        assert_eq!("", after);
-
-        let (before, instr, after) = SedAnnotationParser::get_text_until_next_instruction(&after).unwrap();
+        let (before, instr, after) = SedAnnotationParser::get_text_until_next_instruction(&after, false).unwrap();
         assert_eq!("", before);
         assert_eq!(None, instr);
-        assert_eq!("", after);
+        assert_eq!("end", after);
+
+        let (before, instr, after) = SedAnnotationParser::get_text_until_next_instruction(&after, false).unwrap();
+        assert_eq!("", before);
+        assert_eq!(None, instr);
+        assert_eq!("end", after);
     }
 }
