fbcdb01bdd8ce121d8d73e1b3ef7bde02f7f418d
Petr Šťastný <petr@soptik.tech>
2020-10-02 01:38:06 +0200

    Changed musl to gnu compilation target

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 cargo publish
-cargo build --release --target x86_64-unknown-linux-musl
-mv target/x86_64-unknown-linux-musl/release/desed target/x86_64-unknown-linux-musl/release/desed-x86_64-unknown-linux-musl
-strip target/x86_64-unknown-linux-musl/release/desed-x86_64-unknown-linux-musl
+cargo build --release --target x86_64-unknown-linux-gnu
+mv target/x86_64-unknown-linux-gnu/release/desed target/x86_64-unknown-linux-gnu/release/desed-x86_64-unknown-linux-gnu
+strip target/x86_64-unknown-linux-gnu/release/desed-x86_64-unknown-linux-gnu
 echo "Binary ready"
